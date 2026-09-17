c73495096b9ab8ef4452615cb4592c06d821418c
dlkipotk1-commits <dlkipotk1@gmail.com>
2026-09-17 11:43:43 +0500

    Update CSS styles for social and action elements

--- app.py
+++ app.py
@@ -335,6 +335,25 @@ details summary:hover {{ background:#181818; border-color:rgba(255,77,0,.65); bo
     vertical-align:middle;
     margin-right:5px;
 }}
+.top-actions a img,
+.social img {{
+    width:22px;
+    height:22px;
+    object-fit:contain;
+    vertical-align:middle;
+    margin-right:5px;
+}}
+
+.social {{
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    gap:5px;
+}}
+
+.social img {{
+    margin-right:0;
+}}
 
 </style>
 </head>
